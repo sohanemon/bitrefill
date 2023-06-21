ff8f8294b5a8181057225945af24d5b271f6a0a0
sohanemon <m.sohanemon@gmail.com>
2023-06-21 22:03:26 +0600

    feat: calculation done

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -37,9 +37,9 @@ export default function Modal({ children }) {
                 </p>
               </div>
             ))}
-            <div className='flex items-center justify-between'>
+            <div className='flex items-center justify-between pt-4'>
               <p className='text-theme-dark'>Total</p>
-              <p className='font-medium text-right text-black'>
+              <p className='font-medium text-right text-black '>
                 ${cart.reduce((p, c) => p + c.amount * c.min, 0)}
               </p>
             </div>
