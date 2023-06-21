87632209575a5058012498007bd8ea3587f22f42
sohanemon <m.sohanemon@gmail.com>
2023-06-21 13:57:33 +0000

    Jun 21, 2023, 7:57 PM

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,8 @@
+import useState from '../state';
 import Modal from './modal';
 
 export default function Navbar() {
+  const { cart } = useState();
   return (
     <nav className='flex items-center justify-between p-6 bg-white shadow md:px-60 md:py-8'>
       <img src='/assets/logo.svg' alt='' />
@@ -8,9 +10,11 @@ export default function Navbar() {
         <div className='relative z-20 flex items-center gap-4 cursor-pointer'>
           <img src='/assets/cart.svg' alt='' />{' '}
           <p className='hidden font-semibold md:block text-theme-dark'>Cart</p>
-          <div className='absolute top-0 grid w-4 h-4 text-xs font-semibold text-white rounded-full ring-2 left-5 ring-white place-content-center bg-theme-pink'>
-            2
-          </div>
+          {!!cart.length && (
+            <div className='absolute top-0 grid w-4 h-4 text-xs font-semibold text-white rounded-full ring-2 left-5 ring-white place-content-center bg-theme-pink'>
+              {cart.length}
+            </div>
+          )}
         </div>
       </Modal>
     </nav>
