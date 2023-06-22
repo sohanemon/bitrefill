d42deee357ea4d435ae1e3b86bd8cedaec55db35
sohanemon <m.sohanemon@gmail.com>
2023-06-22 07:30:11 +0000

    Jun 22, 2023, 1:30 PM

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -7,8 +7,15 @@ export default function Product({ image, name, id, range: { min, max } }) {
   }
 
   return (
-    <div className='flex flex-col overflow-hidden ' onClick={handleAddToCard}>
-      <img className='object-contain bg-white rounded grow ' src={image} />
+    <div
+      title='Add to cart'
+      className='flex flex-col overflow-hidden group'
+      onClick={handleAddToCard}
+    >
+      <img
+        className='object-contain bg-white rounded group-hover:opacity-80 grow '
+        src={image}
+      />
       <h3 className='text-lg font-semibold '>{name}</h3>
       <div className='flex items-center gap-2 text-theme-gray'>
         <p>${min}</p> {'-'}
